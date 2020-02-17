974133cf04a64d229f0bedc4d17b4f4a5c088ce3
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-02-17 14:52:41 -0700

    Add icons for true and flase checks, add styling to Tweet component

--- src/components/Tweet/Tweet.js
+++ src/components/Tweet/Tweet.js
@@ -1,25 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
 
 export const Tweet = () => {
 
   return(
     <View style={styles.tweetContainer}>
-      <Text>This is the tweet container</Text>
+      <Text style={styles.headerText}>Confirm Your Tweet:</Text>
+      <Image 
+        style={styles.confirmedFalse} 
+        source={require('../../../assets/confirm.png')} 
+      />
       <TextInput
-      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        multiline
+        maxLength='280'
+        style={styles.tweetInput}
+        placeholder='Your Tweet'
       >
-
       </TextInput>
+      <Button title='Confirm' />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  headerText: {
+    fontSize: 35,
+  },
   tweetContainer: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
     justifyContent: 'center',
+  },
+  tweetInput: {
+    borderColor: 'gray', 
+    borderWidth: 1, 
+    height: 150,
+    marginTop: 10,
+    padding: 10,
+    width: 350, 
+  },
+  confirmedFalse: {
+    height: 200,
+    margin: 50,
+    width: 200,
   }
 })
